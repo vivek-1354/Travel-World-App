144a4e23ce47d6e06c4eee2eba80e69d3df0f22d
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2024-06-27 20:04:02 +0530

    added login route

--- model/user.model.js
+++ model/user.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    number: { type: Number, required: true },
-    email: { type: String, required: true },
+    number: { type: Number, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 })
 
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -15,4 +15,22 @@ router.post('/register', (req, res) => {
         .catch(err => res.status(400).json(err));
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({ number: req.body.number })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            const bytes = cryptoJs.AES.decrypt(user.password, 'mySecretKey');
+            const originalPassword = bytes.toString(cryptoJs.enc.Utf8);
+            if (originalPassword !== req.body.password) {
+
+                return res.status(400).json({ message: 'Invalid credentials' });
+            }
+            const accessToken = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, 'mySecretKey', { expiresIn: '1h' });
+            const { password, ...info } = user._doc;
+            res.json({ ...info, accessToken });
+        })
+})
+
 module.exports = router;            
