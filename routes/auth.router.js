e68b951efe7689151940fb5f38ccf64cb16e4753
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2024-06-27 19:45:00 +0530

    used crypto-js to encrypt the user password

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -2,9 +2,13 @@ const express = require('express');
 const User = require('../model/user.model');
 const router = express.Router();
 
+const cryptoJs = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
 
 router.post('/register', (req, res) => {
-    const { username, number, email, password } = req.body;
+    const { username, number, email } = req.body;
+    const password = cryptoJs.AES.encrypt(req.body.password, 'mySecretKey').toString();
     const user = new User({ username, number, email, password });
     user.save()
         .then(user => res.json(user))
